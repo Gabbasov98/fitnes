575a852fbdfa9323ece31b80e1ab0f2fec864a8b
Marsel Gabbasov <marsel13_98@mail.ru>
2021-09-20 09:37:54 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,58 @@
+function slider() {
+    var swiper = new Swiper('.result .swiper-container', {
+        slidesPerView: 5,
+        spaceBetween: 0,
+        centeredSlidesBounds: true,
+        pagination: {
+            el: '.result .swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 3,
+                spaceBetween: 0
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 0
+            },
+            1200: {
+                slidesPerView: 5,
+                spaceBetween: 0
+            },
+        }
+    })
+}
+
+$(document).ready(function() {
+    slider()
+
+    $(".header__burger").click(function() {
+        $(this).toggleClass("header__burger--active")
+        $(".nav").slideToggle()
+    })
+
+    $(".nav__item").click(function(event) {
+        event.preventDefault();
+        var idc = $(this).attr('href'),
+            top = $(idc).offset().top;
+        $('body,html').animate({ scrollTop: top - 165 }, 500);
+        if ($(window).innerWidth() < 992) {
+            $(".nav").hide()
+        }
+    });
+    // $(window).scroll(function() {
+    //     var $sections = $('section');
+    //     $sections.each(function(i, el) {
+    //         var top = $(el).offset().top - 165;
+    //         var bottom = top + $(el).height();
+    //         var scroll = $(window).scrollTop();
+    //         var idn = $(el).attr('id');
+    //         if (scroll > top && scroll < bottom) {
+    //             $('a.nav__item--active').removeClass('nav__item--active');
+    //             $('a[href="#' + idn + '"]').addClass('nav__item--active');
+    //         }
+    //     });
+    // });
+})
